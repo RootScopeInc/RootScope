5dc0c0923fd0020bccc902146484317b03759630
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2025-10-20 00:32:29 -0400

    Direct latest release pkg via latest tag (#3)

--- install_rootscope.sh
+++ install_rootscope.sh
@@ -10,13 +10,14 @@ fi
 . /etc/os-release
 
 VERSION_CLEAN=$(echo "$VERSION_ID" | cut -d'.' -f1)
+PKG_DIST=""
 
 case "$ID-$VERSION_CLEAN" in
-    debian-12)  PKG="rootscope_1.1.5-1.debian12_amd64.deb";;
-    debian-11)  PKG="rootscope_1.1.5-1.debian11_amd64.deb";;
-    ubuntu-22)  PKG="rootscope_1.1.5-1.ubuntu22.04_amd64.deb";;
-    ubuntu-24)  PKG="rootscope_1.1.5-1.ubuntu24.04_amd64.deb";;
-    ubuntu-25)  PKG="rootscope_1.1.5-1.ubuntu25.04_amd64.deb";;
+    debian-12)  PKG_DIST="debian12";;
+    debian-11)  PKG_DIST="debian11";;
+    ubuntu-22)  PKG_DIST="ubuntu22.04";;
+    ubuntu-24)  PKG_DIST="ubuntu24.04";;
+    ubuntu-25)  PKG_DIST="ubuntu25.04";;
     *)
         echo "Unsupported OS: $ID $VERSION_ID"
         exit 1
@@ -24,6 +25,7 @@ case "$ID-$VERSION_CLEAN" in
 esac
 
 LATEST_TAG=$(curl -s https://api.github.com/repos/RootScopeInc/RootScope/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+PKG="rootscope_${LATEST_TAG}-1.${PKG_DIST}_amd64.deb"
 URL="https://github.com/RootScopeInc/RootScope/releases/download/$LATEST_TAG/$PKG"
 
 echo "Detected: $PRETTY_NAME"
